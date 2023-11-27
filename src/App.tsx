9ea209ac383d4524ec2cacd2b51433e670ba2066
Christopher Kennedy <ckennedy22@wisc.edu>
2023-11-26 19:06:46 -0600

    Initial React code

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// Author: Christopher Kennedy
+// Date: 11-26-23
+
+import React from 'react';
+
+export default class App extends React.Component {
+    render(): React.ReactNode {
+        return <div>
+            Hi there!
+        </div>;
+    }
+};
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+// Author: Christopher Kennedy
+// Date: 11-26-23
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root') as HTMLElement
+);
